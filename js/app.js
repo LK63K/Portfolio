f24007779d1ff02000d073bd6c15156af19b4464
LUKA <donluka94@gmail.com>
2025-04-29 19:36:44 +0200

    add/cv

--- js/app.js
+++ js/app.js
@@ -1,22 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const menu = document.querySelector(".header-menu");
-    const menuIcon = document.querySelector("header .material-icons");
-    const menuLinks = document.querySelectorAll(".header-menu a");
+  const menu = document.querySelector(".header-menu");
+  const menuIcon = document.querySelector("header .material-icons");
+  const menuLinks = document.querySelectorAll(".header-menu a");
 
+  const toggleMenu = () => {
+    menu.classList.toggle("active");
+    menuIcon.innerHTML = menu.classList.contains("active") ? "close" : "menu";
+  };
 
-    const toggleMenu = () => {
-        menu.classList.toggle("active");
-        menuIcon.innerHTML = menu.classList.contains("active") ? "close" : "menu";
-    };
+  menuIcon.addEventListener("click", toggleMenu);
 
-    
-    menuIcon.addEventListener("click", toggleMenu);
-
-
-    menuLinks.forEach(link => {
-        link.addEventListener("click", () => {
-            menu.classList.remove("active");
-            menuIcon.innerHTML = "menu";
-        });
+  menuLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      menu.classList.remove("active");
+      menuIcon.innerHTML = "menu";
     });
+  });
 });
